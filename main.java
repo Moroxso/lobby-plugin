861e3fe44335b1c10c13ab9cb9f0bbe17ad3983d
Moroxso <89519694+Moroxso@users.noreply.github.com>
2022-07-27 19:39:34 +0400

    Add files via upload

--- main.java
+++ main.java
@@ -4,13 +4,10 @@ import net.md_5.bungee.api.plugin.Plugin;
 
 public final class main extends Plugin {
     @Override
-    public void onEnable() {}
-
-    private void loadCommands(){
+    public void onEnable() {
         ProxyServer.getInstance().getPluginManager().registerCommand(this, new command.Lobby());
         ProxyServer.getInstance().getPluginManager().registerCommand(this, new command.Hub());
-    }
-
+}
     @Override
     public void onDisable() {}
 }
